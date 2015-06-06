342641f8a7ae0a16302c1642aed02500e4a63591
reflash <reflash-ne@yandex.ru>
2015-06-06 14:40:24 +0300

    Initial commit#1

---
+++ TestWebApplication1/Controllers/UniversitiesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestWebApplication1.Database;
+
+namespace TestWebApplication1.Controllers
+{
+    public class UniversitiesController : Controller
+    {
+        private TestStudentDbEntities db = new TestStudentDbEntities();
+
+        // GET: Universities
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Universities.ToListAsync());
+        }
+
+        // GET: Universities/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            University university = await db.Universities.FindAsync(id);
+            if (university == null)
+            {
+                return HttpNotFound();
+            }
+            return View(university);
+        }
+
+        // GET: Universities/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Universities/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title")] University university)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Universities.Add(university);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(university);
+        }
+
+        // GET: Universities/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            University university = await db.Universities.FindAsync(id);
+            if (university == null)
+            {
+                return HttpNotFound();
+            }
+            return View(university);
+        }
+
+        // POST: Universities/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title")] University university)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(university).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(university);
+        }
+
+        // GET: Universities/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            University university = await db.Universities.FindAsync(id);
+            if (university == null)
+            {
+                return HttpNotFound();
+            }
+            return View(university);
+        }
+
+        // POST: Universities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            University university = await db.Universities.FindAsync(id);
+            db.Universities.Remove(university);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
